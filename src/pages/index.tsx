8cd2f8e608610e1e27e00da65b5e14d540a3ac70
Ponk8 <lucas.alencar31@gmail.com>
2023-10-27 19:27:35 -0300

    refactor: CSS separated into a new file, added new layout, and integrated a carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,51 @@
-import { styled } from "../styles";
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react";
 
-const Button = styled("button", {
-  backgroundColor: "$green300",
-  borderRadius: 4,
-  border: 0,
-  padding: "4px 8px",
-  span: {
-    fontWeight: "bold",
-  },
-  "&:hover": {
-    filter: "brightness(0.8)",
-  },
-});
+import shirt1 from "../assets/shirts/shirt-1.png";
+import shirt2 from "../assets/shirts/shirt-2.png";
+import shirt3 from "../assets/shirts/shirt-3.png";
+import shirt4 from "../assets/shirts/shirt-4.png";
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2,
+      spacing: 40,
+    },
+  });
+
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={shirt1} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta 2</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta 3</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt4} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta 4</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
