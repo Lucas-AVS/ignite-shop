4d130624398f400d5dc862c53a64d606426c2bcf
Ponk8 <lucas.alencar31@gmail.com>
2023-11-01 03:20:42 -0300

    feat: Create dynamic route page with data fetched from Stripe API.

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,83 @@
+import { GetStaticProps } from "next";
+import Stripe from "stripe";
+import { stripe } from "../../../lib/stripe";
+import Image from "next/image";
+
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
+
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+    defaultPriceId: string;
+  };
+}
+
+export async function getStaticPaths() {
+  const re = await stripe.products.list();
+  const products = re.data;
+
+  const paths = products.map((product) => ({
+    params: { id: product.id },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(price.unit_amount / 100),
+        description: product.description,
+        defaultPriceId: price.id,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hours
+  };
+};
+
+export default function Product({ product }: ProductProps) {
+  function handleBuyButton() {
+    console.log(product.defaultPriceId);
+  }
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+
+        <p>{product.description}</p>
+
+        <button onClick={handleBuyButton}>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
+}
