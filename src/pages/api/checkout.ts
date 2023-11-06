e963ef38dcf711cabfff97142beb235053ea8674
Ponk8 <lucas.alencar31@gmail.com>
2023-11-06 20:18:57 -0300

    feat: Product details in req.body and redirect to success page

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -5,11 +5,18 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const priceId = "";
+  const { priceId } = req.body;
 
-  const successUrl = `${process.env.NEXT_URL}/success`;
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed." });
+  }
+
+  if (!priceId) {
+    return res.status(400).json({ error: "Price not found." });
+  }
+
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
-
   const checkoutSession = await stripe.checkout.sessions.create({
     success_url: successUrl,
     cancel_url: cancelUrl,
@@ -21,7 +28,6 @@ export default async function handler(
       },
     ],
   });
-
   return res.status(201).json({
     checkoutUrl: checkoutSession.url,
   });
